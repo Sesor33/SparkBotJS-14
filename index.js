1a3a322efce5871df21f0252187f196971bd45a1
Tech E. Coyote <sesor333@aol.com>
2023-10-24 13:08:05 -0400

    moved interaction event to own file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Require the necessary discord.js classes
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
@@ -46,21 +46,21 @@ for (const file of eventFiles) {
 // 	console.log('Ready!');
 // });
 
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+// client.on(Events.InteractionCreate, async interaction => {
+// 	if (!interaction.isChatInputCommand()) return;
 
-	const command = client.commands.get(interaction.commandName);
+// 	const command = client.commands.get(interaction.commandName);
 
-	if (!command) return;
+// 	if (!command) return;
 
-	try {
-		await command.execute(interaction, client);
-	}
-	catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
+// 	try {
+// 		await command.execute(interaction, client);
+// 	}
+// 	catch (error) {
+// 		console.error(error);
+// 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+// 	}
+// });
 
 // Login to Discord with your client's token
 client.login(token);

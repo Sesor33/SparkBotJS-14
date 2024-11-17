323ddbaf4db48e80107a1cb2bceefad936765699
Tech E. Coyote <sesor333@aol.com>
2024-11-17 17:38:11 -0500

    Remove debug code thats not needed

--- index.js
+++ index.js
@@ -49,7 +49,6 @@ const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'
 for (const file of eventFiles) {
 	const filePath = path.join(eventsPath, file);
 	const event = require(filePath);
-	console.log(event.name);
 	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args));
 	}
